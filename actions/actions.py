07c367bf1114dd4451b1cf504a48dee15cc014c3
Sonam Pankaj <sonampankaj2@gmail.com>
2023-07-31 13:40:12 +0200

    first commit

--- actions/actions.py
+++ actions/actions.py
@@ -5,7 +5,8 @@ import requests
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
-
+import openai 
+import json
 
 class RestaurantAPI(object):
 
@@ -43,6 +44,34 @@ class ChatGPT(object):
             json=body,
         )
         return result.json()["choices"][0]["message"]["content"]
+    
+
+def ask_distance(restaurant_list):
+    content = "measure the least distance with each given restaurant" +'/n/n' + restaurant_list
+    completion = openai.ChatCompletion.create(
+    model="gpt-4-0613",
+    messages=[{"role": "user", "content": content}],
+    functions=[
+    {
+        "name": "get_measure",
+        "description": "Get the least distance",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "list of all the restaurants and distances as a dictionary(restuarant_name:distance)",
+                },
+            },
+            "required": ["distance"],
+        },
+    }
+        ],
+        function_call={"name":"get_measure"}
+    )
+    return completion.choices[0].message
+
+
 
 restaurant_api = RestaurantAPI()
 chatGPT = ChatGPT()
@@ -64,6 +93,13 @@ class ActionShowRestaurants(Action):
         return [SlotSet("results", results)]
 
 
+def get_distance(d):
+    d = json.loads(d)
+    for i in d.keys():
+        d[i]= float(d[i])
+    t = min(d, key =d.get)
+    return t
+
 class ActionRestaurantsDetail(Action):
     def name(self) -> Text:
         return "action_restaurants_detail"
@@ -76,3 +112,18 @@ class ActionRestaurantsDetail(Action):
         question = tracker.latest_message["text"]
         answer = chatGPT.ask(previous_results, question)
         dispatcher.utter_message(text = answer)
+
+
+class ActionRestaurantsDistance(Action):
+    def name(self) -> Text:
+        return "action_distance"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        previous_results = tracker.get_slot("results")
+        func_calling= ask_distance(previous_results)
+        reply_content = func_calling.to_dict()['function_call']['arguments']
+        distance = json.load(reply_content)['distance']
+        dispatcher.utter_message(text = get_distance(distance))
